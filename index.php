c3269b575dfa174139474e484d2a5118d23669cf
taras-mytsak <56294260+taras-mytsak@users.noreply.github.com>
2019-11-12 00:55:01 +0200

    1

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Auto list</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-treeview/1.2.0/bootstrap-treeview.min.css" />
+</head>
+<body>
+<br>
+<br>
+<div class="container">
+    <h2 align="center">Auto list</h2>
+    <br>
+    <div id="treeview"></div>
+</div>
+
+<script src="js.js"></script>
+</body>
+</html>
---
+++ js.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $.ajax({
+        url:"fetch.php",
+        method: "POST",
+        dataType: "json",
+        success: function(data)
+        {
+            $('#treeview').treeview({data:data});
+        }
+    });
+});
